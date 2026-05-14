1bde7df4eb0c9d9741221c080c737828a27c6929
giovanavbs <giovanavg13@gmail.com>
2026-05-14 14:49:53 -0300

    notificação pasta + localstorage pasta

--- tela-gerar/script-menu.js
+++ tela-gerar/script-menu.js
@@ -10,42 +10,50 @@ const cancelar = document.getElementById("cancelarLogin");
 
 const cancelar2 = document.getElementById("cancelarLogin");
  
+// notificação
+const botaoPDF = document.getElementById("btN3");
+const notificacao = document.getElementById("notificacaoPDF");
+const notificacaoPasta = document.getElementById("notificacaoPasta");
  
 botao.addEventListener("click", function(event){
- 
-event.preventDefault();
- 
-popup.classList.remove("disable");
- 
+    event.preventDefault();
+    popup.classList.remove("disable");
 });
  
  
 cancelar.addEventListener("click", function(){
- 
-popup.classList.add("disable");
- 
-});
+    popup.classList.add("disable");
+ });
 
  
-entrar.addEventListener("click", function(){
+entrar.addEventListener("click", function(event){
+    let usuario = usuarioInput.value;
+    let senha = senhaInput.value;
  
-let usuario = usuarioInput.value;
-let senha = senhaInput.value;
- 
-if(usuario === "admin" && senha === "123"){
-localStorage.setItem("pastaCriada", "true")
-window.location.href = "/tela-galeria/telagaleria.html";
- 
-}else{
- 
-alert("usuario ou senha incorretos!");
-//window.location.href = "telagerar.html";
-}
- 
+    if(usuario === "admin" && senha === "123"){
+        const pasta = localStorage.getItem("pastaCriada");
+
+        if (!pasta) {
+            localStorage.setItem("pastaCriada", "true");
+            
+            popup.classList.add("disable");
+            
+            notificacaoPasta.classList.remove("disable");
+
+            setTimeout(function() {
+                window.location.href = "/tela-galeria/telagaleria.html";
+            }, 2000);
+        } else {
+            window.location.href = "/tela-galeria/telagaleria.html";
+        }
+    }else{
+        alert("usuario ou senha incorretos!");
+        //window.location.href = "telagerar.html";
+    }
 });
  
-/*
-document.addEventListener("DOMContentLoaded", function() {
+
+/*document.addEventListener("DOMContentLoaded", function() {
     //const botao = document.getElementById("btN4");
     
     if (localStorage.getItem("pastaCriada") === "true") {
@@ -57,9 +65,6 @@ document.addEventListener("DOMContentLoaded", function() {
 */
 
 // notificação
-const botaoPDF = document.getElementById("btN3");
-const notificacao = document.getElementById("notificacaoPDF");
-
 botaoPDF.addEventListener("click", function(event) {
     event.preventDefault(); 
 
@@ -72,4 +77,8 @@ botaoPDF.addEventListener("click", function(event) {
 
 notificacao.addEventListener("click", function() {
     window.location.href = "/tela-pdf/telapdf.html";
+});
+
+notificacaoPasta.addEventListener("click", function() {
+    window.location.href = "/tela-galeria/telagaleria.html";
 });
